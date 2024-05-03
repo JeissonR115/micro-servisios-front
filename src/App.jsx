45204757d8d46078ed4dbcb834c1449b37ddf765
jeissonR115 <jeissonfeliper@gmail.com>
2024-05-03 01:03:57 -0500

    consultar datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [data, setData] = useState('');
+    const [findData, setFindData] = useState(false);
+    const [username, setUsername] = useState('');
+    const [error, setError] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:3002/api/consultar', {
+                Nombre1: username,
+            });
+
+            setData(response.data);
+            setFindData(data != '');
+            setError('');
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                setError('Credenciales inválidas');
+            } else {
+                console.error('Error en la solicitud de inicio de sesión:', error);
+            }
+        }
+    };
+
+    return (
+        <div className="fondo">
+            <div className="form-container">
+                <h1 className="form-title">User consultar</h1>
+                <form className="form">
+                    <div className="input-group">
+                        <input type="text" placeholder="Username" value={username} onChange={handleUsernameChange} />
+                        <span className="input-icon"><i className="fas fa-user"></i></span>
+                    </div>
+                    {error && <div className="error-message">{error}</div>}
+                    <button className="form-button" type="button" onClick={handleLogin}>Consultar</button>
+                    
+                    {/* Conditionally render user data */}
+                    {findData? (
+                        <div className="user-data">
+                            <p><span>Documento: </span>{data.Documento}</p>
+                            <p><span>Nombre: </span>{data.Nombre1}</p>
+                            <p><span>Segundo Nombre: </span>{data.Nombre2}</p>
+                            <p><span>Apellido: </span>{data.Apellido1}</p>
+                            <p><span>Segundo Apellido: </span>{data.Apellido2}</p>
+                            <p><span>Correo: </span>{data.Correo}</p>
+                            <p><span>Teléfono: </span>{data.Telefono}</p>
+                        </div>
+                    ):<p>No se encontraron datos </p>}
+                </form>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
