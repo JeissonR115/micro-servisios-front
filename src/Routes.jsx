5e51645b10ab605ab13f8df0d26ce0b3b83ba955
jeissonR115 <jeissonfeliper@gmail.com>
2024-05-03 23:18:17 -0500

    add eliminate

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import Header from './components/Header';
 import Consult from './pages/Consult';
 import Insert from './pages/Insert';
+import Eliminate from './pages/Eliminate';
 
 const AppRoutes = () => {
     return (
@@ -12,7 +13,7 @@ const AppRoutes = () => {
                 <Route path="/*" element={<App />} />
                 <Route path="/Insertar" element={<Insert url='http://localhost:3001/api/insertar' />} />
                 <Route path="/Consultar" element={<Consult url='http://localhost:3002/api/consultar'/>} />
-                <Route path="/Eliminar" element={<App />} />
+                <Route path="/Eliminar" element={<Eliminate url='http://localhost:3003/api/eliminar'/>} />
                 <Route path="/Actualizar" element={<App />} />
             </Routes>
         </BrowserRouter>
---
+++ src/pages/Eliminate/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Form from '../../components/Form';
+
+
+function Eliminate({ url }) {
+    const [data, setData] = useState('');
+    const [userDoc, setUserDoc] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleChange = (e) => {
+        setUserDoc(e.target.value);
+    };
+
+    const handleConsult = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.delete(url, {
+                Documento: userDoc,
+            });
+            setData(response.data);
+            alert("usuario eliminado correctamente")
+            
+        } catch (error) {
+            setErrorMessage('Error al borrar los datos');
+            console.error('Error en la solicitud de inicio de sesión:', error);
+        }
+    };
+
+    const inputFields = [{ label: 'Documento', name: 'Documento', type: 'number', value: userDoc }];
+
+    return (
+        <>
+            <Form
+                inputFields={inputFields}
+                onSubmit={handleConsult}
+                onChange={handleChange}
+                errors={{}}
+            />
+            {errorMessage && <div className='response response__consult'>{errorMessage}</div>}
+        </>
+    );
+}
+
+export default Eliminate;
