26e5f1f0b82655f2356e296f055de76e81c4363c
Kevin-879 <ramirez105.kr@gmail.com>
2024-05-03 22:27:46 -0500

    stilos

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./styles.css";
 class Navbar extends React.Component {
   render() {
     return (
--- src/pages/Insert/index.jsx
+++ src/pages/Insert/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import "./style.css";
 
 const Insert = () => {
     const [userData, setUserData] = useState({
