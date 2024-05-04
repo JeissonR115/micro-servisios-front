708973092a92c816d2ce830c1f8f37e43ccc7479
jeissonR115 <jeissonfeliper@gmail.com>
2024-05-03 23:36:44 -0500

    add update

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Consult from './pages/Consult';
 import Insert from './pages/Insert';
 import Eliminate from './pages/Eliminate';
-
+import Update from './pages/Update';
 const AppRoutes = () => {
     return (
         <BrowserRouter >
@@ -14,7 +14,7 @@ const AppRoutes = () => {
                 <Route path="/Insertar" element={<Insert url='http://localhost:3001/api/insertar' />} />
                 <Route path="/Consultar" element={<Consult url='http://localhost:3002/api/consultar'/>} />
                 <Route path="/Eliminar" element={<Eliminate url='http://localhost:3003/api/eliminar'/>} />
-                <Route path="/Actualizar" element={<App />} />
+                <Route path="/Actualizar" element={<Update url='http://localhost:3004/api/actualizar' />} />
             </Routes>
         </BrowserRouter>
     );
